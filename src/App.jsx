69298845385b657909b19cbfdd0d4c802e885fcf
Siulppe King <elchancateclas@gmail.com>
2025-11-12 19:53:25 -0500

    Add Button & CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from './components/button/Button'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const onClick = () => {
+    alert("¡Botón peligroso clickeado!")
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-container">
+      <h1>Botones personalizados</h1>
+      <div style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
+        <Button>Primary</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="danger" onClick={onClick}>Danger</Button>
+        <Button size="lg">Grande</Button>
+        <Button disabled size='sm'>Deshabilitado</Button>
+        <Button size="lg">Grande El Titulo del Button</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Button.css";
+
+const Button = ({
+    children,
+    variant = "primary",
+    size = "md",
+    disabled = false,
+    onClick
+}) => {
+    const className = `btn btn-${variant} btn-${size}`;
+    return (
+        <button className={className} disabled={disabled} onClick={onClick}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
